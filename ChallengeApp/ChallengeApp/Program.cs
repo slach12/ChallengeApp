63cef7b3ea96c02edda2dcc6a853bd84ddae9fa7
Sławomir Łach <slach12@wp.pl>
2023-02-04 14:13:29 +0100

    Praca domowa z piątego dnia

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,34 +1,61 @@
 ﻿
+//Praca domowa z piątego dnia
+int number  = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToCharArray();
+Console.WriteLine(numberInString);
+Console.WriteLine(letters);
+int[] digits = new int[10];
 
-int[] grades = new int[5];
-
-//string[] dayOfWeeks = new string[7];
-List<string> dayOfWeeks = new List<string>
+for(int i = 0; i<digits.Length;i++)
 {
-    "Poniedziałek",
-    "Wtorek",
-    "Środa",
-    "Czwartek",
-    "Piątek",
-    "Sobota",
-    "Niedziela"
-};
-
-Console.WriteLine(dayOfWeeks[2]);
-
-string[] weekdays = { "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela" };
-
-Console.WriteLine(weekdays[0]);
-
-/*for(int i = 0;i < dayOfWeeks.Count; i++)
-{
-    Console.WriteLine(dayOfWeeks[i]);
+    Console.WriteLine(digits[i]);
 }
-*/
-
-foreach(var day in dayOfWeeks)
+foreach (char c in letters)
 {
-    Console.WriteLine(day);
+    Console.WriteLine(c);   
+    switch (c)
+    {
+        case '0':
+            digits[0]++;
+            break;
+        case '1':
+            digits[1]++;
+            break;
+        case '2':
+            digits[2]++;
+            break;
+        case '3':
+            digits[3]++;
+            break;
+        case '4':
+            digits[4]++;
+            break;
+        case '5':
+            digits[5]++;
+            break;
+        case '6':
+            digits[6]++;
+            break;
+        case '7':
+            digits[7]++;
+            break;
+        case '8':
+            digits[8]++;
+            break;
+        case '9':
+            digits[9]++;
+            break;
+    }
+}
+
+Console.WriteLine("Praca domowa");
+Console.WriteLine($"Wynik dla liczby : {number}");
+
+for(int index =0; index<digits.Length;index++)
+{
+   Console.WriteLine($"{index} => {digits[index]}");
+
 }
 
 
