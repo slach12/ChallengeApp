3bb08510a85f030907146d11cfc2b074653cd5fa
Sławomir Łach <slach12@wp.pl>
2023-02-04 17:47:00 +0100

    Dzień 6 metody i klasy

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,53 +1,31 @@
-﻿
-//Praca domowa z piątego dnia
-int number  = 4566;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToCharArray();
-int[] digits = new int[10];
+﻿using System.Globalization;
+
+User user1 = new User("Adam");
+User user2 = new User("Zuzia");
+User user3 = new User("Damian");
+User user4 = new User("Monika");
 
 
-foreach (char c in letters)
-{  
-    switch (c)
+class User
+{
+    
+  
+
+    public string Login { get; private set; }
+    public string Password { get; private set; }
+    public User()
     {
-        case '0':
-            digits[0]++;
-            break;
-        case '1':
-            digits[1]++;
-            break;
-        case '2':
-            digits[2]++;
-            break;
-        case '3':
-            digits[3]++;
-            break;
-        case '4':
-            digits[4]++;
-            break;
-        case '5':
-            digits[5]++;
-            break;
-        case '6':
-            digits[6]++;
-            break;
-        case '7':
-            digits[7]++;
-            break;
-        case '8':
-            digits[8]++;
-            break;
-        case '9':
-            digits[9]++;
-            break;
+        this.Login = "-";
+        this.Password = "-";
+            
+    }
+    public User(string loign)
+    {
+        Login = loign;
+    }
+    public User(string login, string pasword) {
+        Login = login;
+        Password = pasword;
     }
 }
-Console.WriteLine("Praca domowa");
-Console.WriteLine($"Wynik dla liczby : {number}");
-for(int index =0; index<digits.Length;index++)
-{
-   Console.WriteLine($"{index} => {digits[index]}");
-
-}
-
 
