87c4c5fa8d000796bd3156395ad0b42ebccc47f3
Sławomir Łach <slach12@wp.pl>
2023-02-12 17:57:53 +0100

    Praca domowa dzień 6.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System.Globalization;
 using ChallengeApp;
-
 User user1 = new User("Sławek",38);
 User user2 = new User("Piotrek",32);
 User user3 = new User("Adam",30);
-
 user1.AddScore(1);
 user1.AddScore(2);
 user1.AddScore(3);
@@ -14,8 +12,6 @@ user2.AddScore(6);
 user3.AddScore(7);
 user3.AddScore(8);
 user3.AddScore(9);
-
-
 List<User> users = new List<User>();
 users.Add(user1);
 users.Add(user2);
