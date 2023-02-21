60c253b6727f346cb436dec0be0cdc0303ce6a89
Sławomir Łach <slach12@wp.pl>
2023-02-21 02:31:01 +0100

    Praca domowa dzień 8

--- ChallengeApp/ChallengeApp.Tests/TypeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -2,17 +2,95 @@
 {
     public class TypeTests
     {
+
         [Test]
-        public void TEST() {
+        public void AreDoubleDiferentWhenDiferentValue()
+        {
             //arrange
-            string number1 = "Sławek";
-            string number2 = "Sławek";
+            double number1 = 2.12;
+            double number2 = 3.12;
+            //act
+
+            //assert
+            Assert.AreNotEqual(number1, number2);
+        }
+
+
+        [Test]
+        public void AreDoubleTheSameWhenSameValue()
+        {
+            //arrange
+            double number1 = 2.12;
+            double number2 = 2.12;
+            //act
+
+            //assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void AreFloatDiferentWhenDiferentValue()
+        {
+            //arrange
+            float number1 = 2.1f;
+            float number2 = 3.1f;
+            //act
+
+            //assert
+            Assert.AreNotEqual(number1, number2);
+        }
+
+
+        [Test]
+        public void AreFloatTheSameWhenSameValue()
+        {
+            //arrange
+            float number1 = 2.1f;
+            float number2 = 2.1f;
+            //act
+
+            //assert
+            Assert.AreEqual(number1, number2);
+        }
+
+
+        [Test]
+        public void AreIntDiferentWhenDiferentValue()
+        {
+            //arrange
+            int number1 = 2;
+            int number2 = 3;
+            //act
+
+            //assert
+            Assert.AreNotEqual(number1, number2);
+        }
+
+
+        [Test]
+        public void AreIntTheSameWhenSameValue()
+        {
+            //arrange
+            int number1 = 2;
+            int number2 = 2;
+            //act
+
+            //assert
+            Assert.AreEqual(number1, number2);
+        }
+
+
+        [Test]
+        public void AreStringTheSameWhenSameValue() {
+            //arrange
+            string name1 = "Sławek";
+            string name2 = "Sławek";
 
 
             //act
 
             //assert
-            Assert.AreEqual(number1, number2);    
+            Assert.AreEqual(name1, name2);    
         }
 
         [Test]
