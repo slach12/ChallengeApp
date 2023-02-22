7511d8c0cf1634789918584ef5cf80078d15f2e4
Sławomir Łach <slach12@wp.pl>
2023-02-22 02:40:45 +0100

    Lekcja 9

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<float> grades = new List<float>();
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public Employee(string name, string surname)
+        {
+           this.Name = name;
+           this.Surname = surname;  
+        }
+
+        public void AddGrades(float grade)
+        {
+            this.grades.Add(grade);
+        }
+
+    }
+}
